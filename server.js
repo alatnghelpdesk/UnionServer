2542782976caaf63cde4cd6351ba12249c38c8e8
alatnghelpdesk <alatng.helpdesk@gmail.com>
2024-10-21 00:05:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "alatng.helpdesk@gmail.com";
 const pass = "eqadjzwtienuwhut";
+// 20 nov
 
 // Middleware
 app.use(express.json());
